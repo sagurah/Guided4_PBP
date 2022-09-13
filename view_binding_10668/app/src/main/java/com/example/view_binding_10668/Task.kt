f83bc408217a139355278b121a8497dea27365c0
sagurah <satyogag@gmail.com>
2022-09-13 13:51:16 +0700

    yoihh

---
+++ view_binding_10668/app/src/main/java/com/example/view_binding_10668/Task.kt
@@ -0,0 +1,3 @@
+package com.example.view_binding_10668
+
+data class Task (val chapter: String, val title: String, val details:String)
---
+++ view_binding_10668/app/src/main/java/com/example/view_binding_10668/TaskList.kt
@@ -0,0 +1,13 @@
+package com.example.view_binding_10668
+
+import com.example.view_binding_10668.Task
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Chapter 1", "Tangi Turu","Memulai Hari Baru"),
+        Task("Chapter 2", "Turu neh","Supaya seger"),
+        Task("Chapter 3", "Adus","Ben ora mambu lur"),
+        Task("Chapter 4", "Seger lurr","Akhirnya bisa mulai beraktivitas"),
+        Task("Chapter 5", "Mangkat kuliyah","Turu ng kelas")
+    )
+}
